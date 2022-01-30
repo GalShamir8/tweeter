fe2baeb1027ad770a426734e2c15db85aee1389d
galshamir8 <galshamir8@gmail.com>
2022-01-30 15:52:24 -0500

    second

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     validates_presence_of :username
     validates_presence_of :password
     validates_format_of :password, :with => /[a-z0-9]{6,20}/
